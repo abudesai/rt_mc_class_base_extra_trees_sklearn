c45c400aedaadef867f229769872b6a55f6402bb
Abu Desai <abhyuday.desai@gmail.com>
2023-01-02 21:38:27 -0800

    change to predict; change to json infer; model_config cleanup; remove infer_file; reorganize explanations if XAI; .txt to failure files

--- app/algorithm/model/mc_classifier.py
+++ app/algorithm/model/mc_classifier.py
@@ -15,10 +15,9 @@ MODEL_NAME = "multi_class_base_extra_trees_sklearn"
 
 class Classifier(): 
     
-    def __init__(self, n_estimators = 200, max_depth=5, 
+    def __init__(self, n_estimators = 250, 
                  min_samples_split = 2, min_samples_leaf = 1, **kwargs) -> None:
         self.n_estimators = int(n_estimators)
-        self.max_depth = int(max_depth)
         self.min_samples_split = int(min_samples_split)
         self.min_samples_leaf = int(min_samples_leaf)
         self.model = self.build_model()     
@@ -27,9 +26,9 @@ class Classifier():
     def build_model(self): 
         model = ExtraTreesClassifier(
             n_estimators = self.n_estimators, 
-            max_depth = self.max_depth, 
             min_samples_split = self.min_samples_split, 
-            min_samples_leaf = self.min_samples_leaf
+            min_samples_leaf = self.min_samples_leaf,
+            bootstrap=True
             )
         return model
     
@@ -38,12 +37,12 @@ class Classifier():
         self.model.fit(train_X, train_y)            
         
     
-    def predict(self, X, verbose=False): 
+    def predict(self, X): 
         preds = self.model.predict(X)
         return preds 
     
     
-    def predict_proba(self, X, verbose=False): 
+    def predict_proba(self, X): 
         preds = self.model.predict_proba(X)
         return preds 
     
@@ -55,7 +54,7 @@ class Classifier():
     def evaluate(self, x_test, y_test): 
         """Evaluate the model and return the loss and metrics"""
         if self.model is not None:
-            return self.model.score(x_test, y_test)        
+            return self.model.score(x_test, y_test)         
 
     
     def save(self, model_path): 
@@ -65,23 +64,14 @@ class Classifier():
 
     @classmethod
     def load(cls, model_path):         
-        svc = joblib.load(os.path.join(model_path, model_fname))
-        # print("where the load function is getting the model from: "+ os.path.join(model_path, model_fname))        
-        return svc
+        model = joblib.load(os.path.join(model_path, model_fname))
+        return model
 
 
 def save_model(model, model_path):
-    # print(os.path.join(model_path, model_fname))
-    joblib.dump(model, os.path.join(model_path, model_fname)) #this one works
-    # print("where the save_model function is saving the model to: " + os.path.join(model_path, model_fname))
-    
+    model.save(model_path)
 
 def load_model(model_path): 
-    try: 
-        model = joblib.load(os.path.join(model_path, model_fname))   
-    except: 
-        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
-            Do you have the right trained model in path: {model_path}?''')
+    model = joblib.load(os.path.join(model_path, model_fname))   
     return model
-
 
